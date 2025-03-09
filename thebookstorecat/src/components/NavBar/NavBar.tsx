7909ef0163b1fcf4b01b6d59d9bf2b3c15ec7509
gabicastrum <g.laurindo@pucrs.edu.br>
2025-03-09 16:12:36 -0300

    feat(alterações de estilização do navbar)

--- thebookstorecat/src/components/NavBar/NavBar.tsx
+++ thebookstorecat/src/components/NavBar/NavBar.tsx
@@ -1,25 +1,131 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
 
-const NavBar: React.FC = () => {
-    return (
-        <nav style={{ textAlign: "center", padding: "10px", backgroundColor: "#f8f9fa" }}>
-            <ul style={{ listStyleType: "none", padding: 0, display: "inline-flex", gap: "20px" }}>
-                <li>
-                    <NavLink to="/" style={{ textDecoration: "none", color: "#007bff" }}>Página Incial</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/sobre" style={{ textDecoration: "none", color: "#007bff" }}>Sobre</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/livros" style={{ textDecoration: "none", color: "#007bff" }}>Livros</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/livros/cadastro" style={{ textDecoration: "none", color: "#007bff" }}>Cadastro de Livros</NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
-};
+const pages = [
+  { lable: "Página Inicial", path: "/" },
+  { lable: "Sobre", path: "/sobre" },
+  { lable: "Livros", path: "/livros" },
+  { lable: "Cadastre seu livro", path: "/livros/cadastro" },
+];
 
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const navigate = useNavigate();
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = (path: string) => {
+    navigate(path);
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="fixed" sx={{ width: "100%" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            The Bookstore Cat
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={() => setAnchorElNav(null)}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page, index) => (
+                <MenuItem
+                  key={index}
+                  onClick={() => handleCloseNavMenu(page.path)}
+                >
+                  <Typography sx={{ textAlign: "center" }}>
+                    {page.lable}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            The Bookstore Cat
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page, index) => (
+              <Button
+                key={index}
+                onClick={() => handleCloseNavMenu(page.path)}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page.lable}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
 export default NavBar;
